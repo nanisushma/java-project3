80de065031774ffa7aa45a0eee7486884420fc4c
Admin <admin@Admins-MacBook-Air.local>
2022-09-08 16:48:46 -1000

    New File Added

---
+++ src/day3/FunctionVersion.java
@@ -0,0 +1,37 @@
+package day3;
+
+public class FunctionVersion {
+
+    public static void main(String[] args) {
+
+    int vivek = 60;
+    int sir10 = 70;
+    add(vivek, sir10);
+    int add1 = addAndRetrun(vivek,sir10);
+
+
+    int ashmit = 50;
+    int prem = 30;
+    add(ashmit,prem);
+    int add2 = addAndRetrun(ashmit,prem);
+
+
+    int sushil = 20;
+    int susanta = 10;
+    add(sushil, susanta);
+    int add3 = addAndRetrun(sushil,susanta);
+
+
+
+    }
+
+    public static int addAndRetrun(int a, int b){
+        int add1 = a + b;
+        return add1;
+    }
+
+    public static void add(int vivek, int sir10){
+        int add1 = vivek + sir10;
+        System.out.print(add1);
+    }
+}
