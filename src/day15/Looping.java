e180f7823938269ff8afe8efd3015e749b9f44c6
Admin <admin@Admins-MacBook-Air.local>
2022-09-19 13:39:41 -1000

    BigNumber Practice

--- src/day15/Looping.java
+++ src/day15/Looping.java
@@ -4,16 +4,16 @@ public class Looping {
 
     public static void main(String[] args) {
 
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
-        System.out.println(6);
-        System.out.println(7);
-        System.out.println(8);
-        System.out.println(9);
-        System.out.println(10);
+        //System.out.println(1);
+       // System.out.println(2);
+       // System.out.println(3);
+        //System.out.println(4);
+        //System.out.println(5);
+       // System.out.println(6);
+       // System.out.println(7);
+       // System.out.println(8);
+        //System.out.println(9);
+       // System.out.println(10);
 
         int i = 11;
 
@@ -35,9 +35,11 @@ public class Looping {
 
         System.out.println("I am out of the do while loop");
 
-        for (int k = 1;; k += 1 ){
+        for (int k = 1; ; k += 1 ){
             System.out.println(k);
-            if(k == 10) break;
+            if(k == 10) {
+               break;
+            }
 
         }
         System.out.println("I am out of for loop");
